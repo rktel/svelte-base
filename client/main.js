ed6b1bb8641fde6a4095167bca55b82d115ca672
rktel <rakinakuy@hotmail.com>
2021-03-03 16:35:16 -0500

    imports

--- client/main.js
+++ client/main.js
@@ -1,1 +1,8 @@
-console.log(`Greetings from ${module.id}!`);
+import App from '../imports/ui/App.svelte';
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+    new App({
+        target: document.body
+    })
+})
